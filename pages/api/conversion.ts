098e8d81ff9fe1bc9d31053ea4f839a8f306e564
Arthur Aleksanyan <arthuralexmd@gmail.com>
2022-11-01 21:26:39 +0400

    discarding changes

--- pages/api/conversion.ts
+++ pages/api/conversion.ts
@@ -30,13 +30,11 @@ const handler = nc<NextApiRequest, NextApiResponse>({
     res.status(404).end("Page is not found");
   },
 })
-const imagePath = path.join(process.cwd() + '/public/images')
-console.log(imagePath);
 
 /*multer setup*/
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, imagePath);
+      cb(null, 'public/images');
   },
   filename: (req, file, cb) => {
       const fileName = file.originalname.toLowerCase().split(' ').join('-');
